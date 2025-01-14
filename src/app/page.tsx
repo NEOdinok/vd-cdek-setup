0340e3ac70dafd1d5dcae529748f5169bd0f7ac8
goshamartynovich <goshamartynovich@gmail.com>
2025-01-14 19:21:47 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col min-h-screen w-screen gap-8 row-start-2 items-center justify-center">
+        <div className="w-full max-w-screen-sm border border-white aspect-square"></div>
+      </main>
+    </div>
+  );
+}
